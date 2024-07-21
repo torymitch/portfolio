0553243f03994e98ff8ce0e0f8e0ec35fd81b379
Tory Mitchell <torymitch@Torys-MBP.fios-router.home>
2024-07-21 12:06:12 -0400

    Updated Toast Message

--- src/components/AboutView.vue
+++ src/components/AboutView.vue
@@ -1,47 +1,46 @@
 <template>
-  <div class="about">
-    <h1 class="pr-5">This is an about page</h1>
-    <v-btn @click="raiseAlert" >Display Details</v-btn>
+  <div>
+    <v-btn @click="raiseAlert">Display About Details</v-btn>
   </div>
 </template>
 
 <script>
-import { toast } from 'vue3-toastify';
-import 'vue3-toastify/dist/index.css';
+import { toast } from 'vue3-toastify'
+import 'vue3-toastify/dist/index.css'
 
 export default {
   data() {
-        return {
-            name: 'Tory Mitchell',
-        }
-    },
+    return {
+      name: 'Tory Mitchell'
+    }
+  },
   computed: {
-        today() {
-          return new Date()
-        },
-        displayProfile() {
-         return `Created On ${this.today} by ${this.name}`;
-        }
+    today() {
+      return new Date()
     },
+    displayProfile() {
+      return `Created On ${this.today} by ${this.name}`
+    }
+  },
   methods: {
-      raiseAlert() {
-        toast(this.displayProfile, {
-          autoClose: 1000,
-        });
+    raiseAlert() {
+      toast(this.displayProfile, {
+        autoClose: 1000
+      })
     },
     mounted() {
-        console.log('Application mounted');
-    },
+      console.log('Application mounted')
+    }
   }
 }
 </script>
 
 <style>
-@media (min-width: 1024px) {
+/* @media (min-width: 1024px) {
   .about {
     min-height: 100vh;
     display: flex;
     align-items: center;
   }
-}
+} */
 </style>
