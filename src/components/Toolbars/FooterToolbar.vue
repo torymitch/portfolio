4b756010eb1bd94bd3b4c44c0cbf3c4f8507f624
Tory Mitchell <torymitch@gmail.com>
2024-07-21 12:14:35 -0400

    adding toolbars

---
+++ src/components/Toolbars/FooterToolbar.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-footer class="w-100 toolbar footer" title="Footer">This Is The Footer</v-footer>
+</template>
+
+<style scoped>
+.footer {
+  background-color: rgb(255, 0, 30);
+  max-height: 10vh;
+}
+</style>
---
+++ src/components/Toolbars/HeaderToolbar.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-card class="h-auto w-100 toolbar"> Put Stuff In This Header </v-card>
+</template>
+
+<style scoped>
+.toolbar {
+  background-color: rgb(255, 0, 30);
+}
+</style>
